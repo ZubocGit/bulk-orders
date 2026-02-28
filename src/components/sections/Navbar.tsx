bb94bcdf8c820338ce52835112d75454d73b4d06
Ayisha | Zuboc <mail@zuboc.com>
2026-02-28 11:24:33 +0530

    logo navigation

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -21,7 +21,13 @@ export function Navbar() {
       }`}
     >
       <div className="container mx-auto px-4 md:px-6 flex items-center justify-between">
-        <a href="zuboc.com">
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            window.history.back();
+          }}
+        >
           <img
             src="assets/Icons/zuboc_logo.svg"
             alt="Zuboc Logo"
